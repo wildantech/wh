4cbe13b8f8e9bad3804744695e5fbcb0dcedac76
wildanululaufa <72399055+wildantech@users.noreply.github.com>
2025-04-22 11:28:07 +0700

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -2,7 +2,8 @@ import { Pool, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
 import ws from "ws";
 import * as schema from "@shared/schema";
-
+import dotenv from "dotenv";
+dotenv.config();
 // Konfigurasi untuk menghubungkan ke database Neon melalui websocket
 neonConfig.webSocketConstructor = ws;
 
@@ -16,3 +17,5 @@ export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 export const db = drizzle({ client: pool, schema });
 
 console.log("DATABASE_URL dari Vercel:", process.env.DATABASE_URL);
+
+
