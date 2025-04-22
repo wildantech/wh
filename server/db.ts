24fb24f9d0ea8e636bde7b7ff441ac3508c07e52
wildanululaufa <72399055+wildantech@users.noreply.github.com>
2025-04-22 11:17:47 +0700

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -15,4 +15,4 @@ if (!process.env.DATABASE_URL) {
 export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 export const db = drizzle({ client: pool, schema });
 
-
+console.log("DATABASE_URL dari Vercel:", process.env.DATABASE_URL);
